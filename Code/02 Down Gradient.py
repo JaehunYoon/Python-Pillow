14b1b020ca0856abe848dd2e5e6d30cd42523f47
JaehunYoon <goodasd123@gmail.com>
2018-03-16 17:41:46 +0900

    02 Down Gradient

---
+++ Code/02 Down Gradient.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw
+
+width, height = input("Please input width and height > ").split()
+width = int(width)
+height = int(height)
+new = Image.new('RGB', (width, height), (0, 0, 0))
+draw = ImageDraw.Draw(new)
+
+r = 0
+g = 0
+b = 0
+
+for i in range(0, height):
+    draw.line((0, i) + (width, i), (r, g, b))
+    r += 1
+    g += 1
+    b += 1
+    if r > 255:
+        r = 255
+    if g > 255:
+        g = 255
+    if b > 255:
+        b = 255
+new.save('02_gradient.png')
