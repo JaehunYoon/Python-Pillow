d9c8bf889c12cd236aabb109d8ccc570c8583e47
JaehunYoon <goodasd123@gmail.com>
2018-04-01 23:23:37 +0900

    ImageDraw module - ellipse example code modified

--- Code/Reference/ImageDraw/Examples/Draw Ellipse/ellipse.py
+++ Code/Reference/ImageDraw/Examples/Draw Ellipse/ellipse.py
@@ -6,8 +6,7 @@ img = Image.new('RGBA', (width, height), (255, 255, 255))
 
 draw = ImageDraw.Draw(img)
 
-for i in range(width, 0, -1):
-    draw.ellipse(((0, 0), (width, height)), (0, 255, 255), (0, 0, 0))
+draw.ellipse(((0, 0), (width, height)), (0, 255, 255), (0, 0, 0))
 
 del draw
 
